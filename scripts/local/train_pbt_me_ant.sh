071ed6bfe9fe42067db8525d6ab91df653626d68
Sumeet Batra <sbatra@nvidia.com>
2023-09-21 21:46:15 -0700

    pbt-me local runners

---
+++ scripts/local/train_pbt_me_ant.sh
@@ -0,0 +1,17 @@
+ENV_NAME="ant_uni"
+GRID_SIZE=10  # number of cells per archive dimension
+
+set -- 1111 2222 3333 4444  # seeds
+
+for item in "$@";
+ do echo "Running seed $item";
+ RUN_NAME="pbt_me_"$ENV_NAME"_baseline_seed_"$item
+ echo $RUN_NAME
+ python -m scripts.train_pbt_me --env_name="$ENV_NAME" \
+                                --seed="$item" \
+                                --run_name="$RUN_NAME" \
+                                --grid_size=$GRID_SIZE \
+                                --num_iterations=10000 \
+                                --num_loops=625 \
+                                --use_wandb=True
+done
--- scripts/local/train_qdpg.sh
+++ scripts/local/train_qdpg.sh
@@ -5,21 +5,21 @@
 # ENV_NAME: which environment to run on (see qdax/environments/__init__.py for a list of all runnable envs)
 # then from the project root dir you can run ./scripts/train_pga_me.sh
 
-ENV_NAME="ant_uni"
+ENV_NAME="halfcheetah_uni"
 GRID_SIZE=10  # number of cells per archive dimension
 
 set -- 1111 2222 3333 4444  # seeds
 
 for item in "$@";
  do echo "Running seed $item";
- RUN_NAME="qdpg_"$ENV_NAME"_baseline_seed_"$item
+ RUN_NAME="qdpg_"$ENV_NAME"_baseline_grid10_seed_"$item
  echo $RUN_NAME
  python -m scripts.train_qdpg --env_name="$ENV_NAME" \
                               --seed="$item" \
                               --run_name=$RUN_NAME \
                               --grid_size=$GRID_SIZE \
                               --episode_length=1000 \
-                              --num_iterations=4000 \
+                              --num_iterations=3000 \
                               --policy_hidden_layer_sizes=128 \
                               --policy_hidden_layer_sizes=128 \
                               --use_wandb=True \
